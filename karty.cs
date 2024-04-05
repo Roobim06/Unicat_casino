01fb7e95ddb391cd6910e5a6ebdfa2c19150669a
kitCat <damig123456789@gmail.com>
2024-04-05 19:43:24 +0200

    Dodałem blackjack i ruletke

---
+++ karty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unicat_Casino
+{
+    class karty
+    {
+        public string symbol;
+        public string numer;
+        public string zdj;
+        public bool odkryte;
+        public karty(string symbol, string numer, string zdj, bool odkryte)
+        {
+            this.symbol = symbol;
+            this.numer = numer;
+            this.zdj = zdj;
+            this.odkryte = odkryte;
+        }
+    }
+}
