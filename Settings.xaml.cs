c37fedf53da68bf474c37cc5495c35f745468b34
klocmaster69 <129272531+klocmaster69@users.noreply.github.com>
2024-04-16 13:30:13 +0200

    Dodałem infinite zetony generator

--- Settings.xaml.cs
+++ Settings.xaml.cs
@@ -23,5 +23,10 @@ namespace Unicat_Casino
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            konta.UpdateTokens(200);
+        }
     }
 }
