218c178464f028dc19cd553c564d03eb2b77dc69
Wiktoria Czarniecka <wiktorunias@wp.pl>
2024-04-22 17:47:07 +0200

    juz prawie meta

--- RollTheDice.xaml.cs
+++ RollTheDice.xaml.cs
@@ -17,12 +17,12 @@ using System.Diagnostics;
 using System.Drawing;
 using static System.Net.Mime.MediaTypeNames;
 using WpfAnimatedGif;
+using System.Printing;
 
 namespace Unicat_Casino
 {
     public partial class RollTheDice : Window
     {
-        // 1 - orzeł, 0 - reszka
         int bet = 0;
         int postawionePieniądze = 0;
         Random random = new Random();
@@ -30,6 +30,10 @@ namespace Unicat_Casino
         public RollTheDice()
         {
             InitializeComponent();
+            foreach (Button button in RollButtons.Children)
+            {
+                button.IsEnabled = false;
+            }
         }
 
         private async void RollClick(object sender, MouseButtonEventArgs e)
@@ -37,21 +41,17 @@ namespace Unicat_Casino
             if(bet != 0){
                 dice.Visibility = Visibility.Collapsed;
                 diceRoll.Visibility = Visibility.Visible;
-
                 int wynikLosowania = random.Next(1, 7);
-                await Task.Delay(1000);
-                wynik1.Text = wynikLosowania.ToString();
-                wynik2.Text = bet == wynikLosowania ? "Gratulacje! Wygrałeś pieniądze!" : "Przegrałeś pieniądze!";
+                await Task.Delay(1500);
+                coWypadlo.Text = "Co wypadło: " + wynikLosowania.ToString();
+                wynik.Text = bet == wynikLosowania ? "Gratulacje! Wygrałeś pieniądze!" : "Przegrałeś pieniądze!";
                 dice.Source = new BitmapImage(new Uri("images/dice/" + wynikLosowania.ToString() + "_dice.png", UriKind.Relative));
-
-                foreach (Button button in RollButtons.Children)
-                {
-                    button.IsEnabled = true;
-                }
-
                 dice.Visibility = Visibility.Visible;
                 diceRoll.Visibility = Visibility.Collapsed;
                 bet = 0;
+                Zetony.IsEnabled = true;
+
+
             }
         }
         private void Roll(object sender, RoutedEventArgs e)
@@ -64,16 +64,26 @@ namespace Unicat_Casino
             Button clickedButton = (Button)sender;
             bet = int.Parse(clickedButton.Content.ToString());
 
-            wynik1.Text = "";
-            wynik2.Text = "";
+            coPostawiles.Text = "Co postawiłeś: " + bet.ToString();
+            Zetony.IsEnabled = false;
+
         }
 
-        private void ReturnMenu(object sender, RoutedEventArgs e)
+        private void ClickCancelButton(object sender, RoutedEventArgs e)
         {
             Menu okno = new Menu();
             okno.Show();
             this.Close();
         }
-
+        private void ZetonyPostawione(object sender, RoutedEventArgs e)
+        {
+            foreach (Button button in RollButtons.Children)
+            {
+                button.IsEnabled = true;
+            }
+            coWypadlo.Text = "";
+            coPostawiles.Text = "";
+            wynik.Text = "";
+        }
     }
 }
